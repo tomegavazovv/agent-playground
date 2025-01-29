35045e63ba3423fcc3b9e969df9460de654d0ef4
Tome Gavazov <tomegavazov@gmail.com>
2025-01-29 17:12:02 +0100

    asd

---
+++ db/db.py
@@ -0,0 +1,16 @@
+from google.cloud import firestore
+from utils.flatten_dict import flatten_dict
+import streamlit as st
+
+credentials_dict = st.secrets['gcp_service_account']
+db = firestore.Client().from_service_account_info(credentials_dict)
+
+def get_jobs(limit=10, offset=0):
+    """Fetch jobs from Firestore with pagination"""
+    jobs_ref = db.collection('jobs').where('category', '==', 'Web Development').limit(limit).offset(offset)
+    jobs = jobs_ref.get()
+    jobs = [flatten_dict(job.to_dict()) for job in jobs]
+    
+    fields = ['description', 'title']
+    
+    return [{field: job.get(field, '') for field in fields} for job in jobs]
