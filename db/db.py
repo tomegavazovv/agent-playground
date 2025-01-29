0d16887b13c97eeb73feaa9b9e5d20a19272f12a
Tome Gavazov <tomegavazov@gmail.com>
2025-01-29 18:02:14 +0100

    sds

--- db/db.py
+++ db/db.py
@@ -1,4 +1,5 @@
 from google.cloud import firestore
+from google.oauth2 import service_account
 from utils.flatten_dict import flatten_dict
 import streamlit as st
 from dotenv import load_dotenv
@@ -7,8 +8,35 @@ import json
 
 load_dotenv()
 
-credentials_dict = json.loads(os.getenv('GOOGLE_CREDENTIALS'))
-db = firestore.Client().from_service_account_info(credentials_dict)
+
+project_id = st.secrets.get("project_id")
+private_key_id = st.secrets.get("private_key_id")
+private_key = st.secrets.get("private_key")
+client_email = st.secrets.get("client_email")
+client_id = st.secrets.get("client_id")
+auth_uri = st.secrets.get("auth_uri")
+token_uri = st.secrets.get("token_uri")
+auth_provider_x509_cert_url = st.secrets.get("auth_provider_x509_cert_url")
+client_x509_cert_url = st.secrets.get("client_x509_cert_url")
+universe_domain = st.secrets.get("universe_domain")
+
+credentials_dict = {
+    "type": "service_account",
+    "project_id": project_id,
+    "private_key_id": private_key_id,
+    "private_key": private_key,
+    "client_email": client_email,
+    "client_id": client_id,
+    "auth_uri": auth_uri,
+    "token_uri": token_uri,
+    "auth_provider_x509_cert_url": auth_provider_x509_cert_url,
+    "client_x509_cert_url": client_x509_cert_url,
+    "universe_domain": universe_domain,
+}
+
+creds = service_account.Credentials.from_service_account_info(credentials_dict)
+db = firestore.Client(credentials=creds, project=project_id)
+
 
 def get_jobs(limit=10, offset=0):
     """Fetch jobs from Firestore with pagination"""
